935a105aef6be29105d219f90aaaf9ae6e658a76
No-Name-exe <51513986+No-Name-exe@users.noreply.github.com>
2025-10-08 20:31:38 +0300

    Made changes to work with new database

--- lab1/app/models.py
+++ lab1/app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -11,8 +12,8 @@ class ApplicationRouter(models.Model):
 		REJECTED = 'отклонён'
 
 	# id = models.IntegerField(primary_key=True)
-	creator = models.CharField(max_length=255)
-	moderator = models.CharField(max_length=255,null=True,blank=True)
+	creator = models.ForeignKey(User, on_delete=models.PROTECT, related_name="applications_created")
+	moderator = models.ForeignKey(User, on_delete=models.PROTECT, null=True,blank=True, related_name="applications_moderated")
 	status = models.CharField(max_length=255, choices=Status.choices, default=Status.DRAFT)
 	date_create = models.DateField()
 	date_modific= models.DateField(null=True,blank=True)
